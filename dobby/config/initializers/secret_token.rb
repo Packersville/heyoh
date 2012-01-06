88d3e3edda12bbcb400559a7c5c5af258be2fd80
Tyler Mangan <tyler.mangan@vanderbilt.edu>
2012-01-06 11:15:56 -0600

    project created

---
+++ dobby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dobby::Application.config.secret_token = '4effd9e0dc61a672b10ff54743cbecbdf2fec45463b9b02be5c19b6fdb6a4b8b64356caf664c64c4b5687a5e3512c6c7fe5e503ffb3123485db3a93cf03c3056'

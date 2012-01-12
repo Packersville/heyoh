99636d06281a638fccfe95b60fdfbbc02e7a39f5
Tyler Mangan <tyler.mangan@vanderbilt.edu>
2012-01-12 15:11:21 -0600

    User can view their picks after submission and update them as well.

--- dobby/app/controllers/games_controller.rb
+++ dobby/app/controllers/games_controller.rb
@@ -2,12 +2,27 @@ class GamesController < ApplicationController
   before_filter :get_current_week
   
   def index
+    get_user_picks
     @games = Game.where("week= '#{@get_current_week}'")
   end
   
   def create
+    remove_user_picks_if_present
     @userspicks = current_user.users_picks.build(params[:users_picks])
+    @userspicks.week = @get_current_week
     @userspicks.save
-    redirect_to games_path(@get_current_week)
+    redirect_to games_path
+  end
+  
+  protected
+  def get_user_picks
+    @users_picks = UsersPick.where("user_id = '#{current_user.id}' AND week = '#{@get_current_week}'").first
+  end
+  
+  def remove_user_picks_if_present
+    picks =  UsersPick.where("user_id = '#{current_user.id}' AND week = '#{@get_current_week}'").first
+    if picks.present?
+      UsersPick.find(picks.id).destroy
+    end
   end
 end
--- dobby/app/helpers/games_helper.rb
+++ dobby/app/helpers/games_helper.rb
@@ -2,5 +2,5 @@ module GamesHelper
   def get_team_name(teamNumber)
     team = Team.find(teamNumber)
     team.name
-  end
+  end 
 end
